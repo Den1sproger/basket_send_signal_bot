54830741ef42764710c69e9eada8f86eee59041d
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-04-12 19:05:16 +0300

    fix: added all necessary imports

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from aiogram import executor
 from telegram_bot import dp
 from telegram_bot.handlers.start.start import *
 from telegram_bot.handlers.admin.signal import *
+from telegram_bot.handlers.admin.user_work import *
+from telegram_bot.handlers.user.add import *
+
 
 
 if __name__ == '__main__':
